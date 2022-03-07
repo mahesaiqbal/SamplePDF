42383c673dbaa791c03544dc0033cfee6588030f
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2022-03-07 11:40:41 +0700

    Add permission to access device storage

--- app/src/main/java/com/mahesaiqbal/samplepdf/MainActivity.kt
+++ app/src/main/java/com/mahesaiqbal/samplepdf/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.mahesaiqbal.samplepdf
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.downloader.Error
 import com.downloader.OnDownloadListener
@@ -18,6 +22,8 @@ import java.io.File
 class MainActivity : AppCompatActivity() {
     
     private lateinit var binding: ActivityMainBinding
+    
+    private val PERMISSION_REQUEST_CODE = 1;
     
     private lateinit var pdfFile: File
     
@@ -49,6 +55,10 @@ class MainActivity : AppCompatActivity() {
     }
     
     private fun downloadPdfFromInternet(url: String, dirPath: String, fileName: String) {
+        // Create a folder to accommodate the downloaded pdf
+        // Check permissions first
+        checkPermissionForCreateDir()
+        
         PRDownloader.download(url, dirPath, fileName).build().start(
             object : OnDownloadListener {
                 override fun onDownloadComplete() {
@@ -107,5 +117,52 @@ class MainActivity : AppCompatActivity() {
             
             startActivity(createChooser)
         }
+    }
+    
+    private fun checkPermissionForCreateDir() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            createSampleDir()
+        } else {
+            askPermission()
+        }
+    }
+    
+    private fun createSampleDir() {
+        val folderName = "SamplePDF Downloads"
+        val file = File(Environment.getExternalStorageDirectory(), folderName)
+    
+        if (!file.exists()) {
+            file.mkdir()
+            Toast.makeText(this, "Successfully created file on: ${file.absolutePath}", Toast.LENGTH_SHORT).show()
+            Log.d("FileUtils", "Successfully created file on: ${file.absolutePath}")
+        } else {
+            Toast.makeText(this, "Folder already exist on: ${file.absolutePath}", Toast.LENGTH_SHORT).show()
+            Log.d("FileUtils", "Folder already exist on: ${file.absolutePath}")
+        }
+    }
+    
+    private fun askPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_CODE
+        )
+    }
+    
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                createSampleDir()
+            } else {
+                Toast.makeText(this,"onRequestPermissionsResult Permission Denied",Toast.LENGTH_SHORT).show()
+                Log.d("MainAct", "onRequestPermissionsResult Permission Denied")
+            }
+        }
+        
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
