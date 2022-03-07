9057ef6a690df78e7257d99314fa0df59d4b6e2e
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2022-03-07 12:18:14 +0700

    Rollback to the previous commit

--- app/src/main/java/com/mahesaiqbal/samplepdf/MainActivity.kt
+++ app/src/main/java/com/mahesaiqbal/samplepdf/MainActivity.kt
@@ -23,8 +23,6 @@ class MainActivity : AppCompatActivity() {
     
     private lateinit var binding: ActivityMainBinding
     
-    private val PERMISSION_REQUEST_CODE = 1;
-    
     private lateinit var pdfFile: File
     
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,10 +53,6 @@ class MainActivity : AppCompatActivity() {
     }
     
     private fun downloadPdfFromInternet(url: String, dirPath: String, fileName: String) {
-        // Create a folder to accommodate the downloaded pdf
-        // Check permissions first
-        checkPermissionForCreateDir()
-        
         PRDownloader.download(url, dirPath, fileName).build().start(
             object : OnDownloadListener {
                 override fun onDownloadComplete() {
@@ -119,14 +113,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
     
-    private fun checkPermissionForCreateDir() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            createSampleDir()
-        } else {
-            askPermission()
-        }
-    }
-    
     private fun createSampleDir() {
         val folderName = "SamplePDF Downloads"
         val file = File(Environment.getExternalStorageDirectory(), folderName)
@@ -139,30 +125,5 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Folder already exist on: ${file.absolutePath}", Toast.LENGTH_SHORT).show()
             Log.d("FileUtils", "Folder already exist on: ${file.absolutePath}")
         }
-    }
-    
-    private fun askPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-            PERMISSION_REQUEST_CODE
-        )
-    }
-    
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                createSampleDir()
-            } else {
-                Toast.makeText(this,"onRequestPermissionsResult Permission Denied",Toast.LENGTH_SHORT).show()
-                Log.d("MainAct", "onRequestPermissionsResult Permission Denied")
-            }
-        }
-        
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
